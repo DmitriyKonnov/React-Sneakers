131b238f5eb065d5c3beb44f4297b513d03d36e4
DmitriyKonnov <dimon170893@gmail.com>
2021-07-04 19:25:31 +0300

    Добавил шапку контента

--- src/App.js
+++ src/App.js
@@ -35,6 +35,17 @@ function App() {
           </div>
         </div>
       </header>
+      <div className="content">
+        <div className="container">
+          <div className="contentHeader">
+            <h3 className="contentTitle">Все кросовки</h3>
+            <div className="searchBlock">
+              <img src="img/search.png" alt="Поиск" />
+              <input type="text" placeholder = "Поиск..."/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
