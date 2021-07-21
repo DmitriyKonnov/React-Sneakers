a76c9d92a3816b642fd6a1e19e6c81de570b1430
DmitriyKonnov <dimon170893@gmail.com>
2021-07-21 10:19:18 +0300

    создал компанент каточки товара

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import ContentHeader from './components/ContentHeader'
 import Card from './components/Card'
 import Cart from './components/Cart'
 
+const arr = [
+  {title: "Мужские Кроссовки Nike Blazer Mid Suede",
+   price: 12999,
+   imageUrl: "/img/sneakers-1.jpg"
+  }]
+
 function App() {
 
   const [ cartOpened, setCartOpened] = React.useState ( false );
@@ -16,10 +22,16 @@ function App() {
         <div className="container">
           <ContentHeader />
           <div className="cardBlock">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {arr.map ( (item) => (
+              <Card 
+                title = {item.title}
+                price = {item.price}
+                imageUrl = {item.imageUrl}
+              />
+            ))
+
+            }
+            <Card />
           </div>
         </div>
       </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,17 @@
 import styles from "./Card.module.scss"
 
-function Card () {
+function Card (props) {
     return (
         <div className={styles.card}>
             <button className = {styles.cardFavorite}>
                 <img src="img/favorite-sneakers.svg" alt="Избранное" />
             </button>
-            <img className = {styles.cardImg} src="img/sneakers-1.jpg" alt="Кросовки" />
-            <h5 className={styles.cardTitle}>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img className = {styles.cardImg} src= {props.imageUrl} alt="Кросовки" />
+            <h5 className={styles.cardTitle}>{props.title}</h5>
             <div className={styles.cardFooter}>
                 <div className={styles.cardPrice}>
                     <span className = {styles.text}>Цена:</span>
-                    <span className = {styles.price}>12 999 руб.</span>
+                    <span className = {styles.price}>{props.price} руб.</span>
                 </div>
                 <button className = {styles.cardAdd}>
                     <img src="img/add.svg" alt="Добавить" />
